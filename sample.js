d1bcc88b8ffb00123e364fcbe70d469c15bfccb7
Anna Zaldivar <anna.zaldivar@gmail.com>
2025-08-05 19:21:02 -0700

    intro stuff

---
+++ sample.js
@@ -0,0 +1,40 @@
+var difference = 45 -33; 
+console.log (difference);
+
+var mydecimal= 1.2 + 4.4;
+console.log(mydecimal);
+
+var remainder;
+remainder= 11 %3;
+console.log(remainder);
+
+var a=3;
+var b=1;
+a+=2;
+b+=7;
+console.log(a+b);
+
+var myFirstName= "Anna";
+var myLastName= "Casey"; 
+console.log( "My name is "+ myFirstName +" "+ myLastName);
+
+var ourStr= "I come first";
+ourStr += "I come second";
+
+var myStr= "This is the first sentence";
+myStr +="This is my second sentence";
+
+console.log(`${ourStr}  ${myStr}`)
+
+var numbernotation="";
+var aName="Ada";
+var bName="Garcia"
+numbernotation= aName[1];
+numbernotation= bName[3];
+console.log(numbernotation+ numbernotation);
+
+var myArray= [18,60,78];
+myArray[0]=13;
+console.log(myArray);
+myArray.pop(2);
+console.log(myArray);
